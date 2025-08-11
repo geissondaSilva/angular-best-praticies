3499a43e3cf48529cce839904310db0066c351dd
Geisson <geisonmsilva12@gmail.com>
2025-08-11 20:06:57 -0300

    Criado exemplo de lista não seguindo as boas práticas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  dataHora = new Date();
+
+  constructor() {
+    setInterval(() => {
+      this.dataHora = new Date();
+    }, 1000);
+  }
 }
